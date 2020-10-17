3319804892c8e52ec90752ba430ce5c3a6f6f038
Pbihao <1435343052@qq.com>
2020-10-17 19:19:01 +0800

    初步完成头像的选择图片功能还没有写回调函数

--- app/src/main/java/com/example/hxchat/ui/fragment/userinfo/meUserInfoFragment.kt
+++ app/src/main/java/com/example/hxchat/ui/fragment/userinfo/meUserInfoFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.blankj.utilcode.util.PermissionUtils.permission
 import com.example.hxchat.R
 import com.example.hxchat.app.Constants
 import com.example.hxchat.app.base.BaseFragment
@@ -23,7 +24,15 @@ import kotlinx.android.synthetic.main.home_toolbar.*
 import me.hgj.jetpackmvvm.ext.download.ShareDownLoadUtil.putBoolean
 import me.hgj.jetpackmvvm.ext.navigateAction
 import me.hgj.jetpackmvvm.ext.parseState
-import java.util.jar.Manifest
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import androidx.core.content.ContextCompat
+import com.tbruyelle.rxpermissions2.RxPermissions
+import java.io.File
 
 /**
  *Created by Pbihao
@@ -59,6 +68,59 @@ class meUserInfoFragment: BaseFragment<MeUserInfoViewModel, FragmentMeUserInfoBi
         })
     }
 
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d("select", "选择了一张图片")
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode ==  RESULT_OK){
+            when(requestCode){
+                Constants.REQ_SELECT_PHOTO -> cropRawPhoto(obtainSelectPhoto(data))
+            }
+        }
+    }
+
+    private fun cropRawPhoto(uri: Uri?) {
+        Log.d("select", "选择了一张图片")
+        /*
+        uri?.let {
+            val options = UCrop.Options()
+            //        options.setToolbarTitle("裁剪");
+            // 修改标题栏颜色
+            options.setToolbarColor(ContextCompat.getColor(context,R.color.colorPrimary))
+            options.setToolbarWidgetColor(ContextCompat.getColor(context,R.color.white))
+            // 修改状态栏颜色
+            options.setStatusBarColor(ContextCompat.getColor(context,R.color.colorPrimaryDark))
+            // 隐藏底部工具
+            options.setHideBottomControls(true)
+            // 图片格式
+            options.setCompressionFormat(Bitmap.CompressFormat.JPEG)
+            // 设置图片压缩质量
+            options.setCompressionQuality(100)
+            // 是否让用户调整范围(默认false)，如果开启，可能会造成剪切的图片的长宽比不是设定的
+            // 如果不开启，用户不能拖动选框，只能缩放图片
+            options.setFreeStyleCropEnabled(false)
+            options.setCircleDimmedLayer(true)
+            avatarFile = File(getApp().getPath(), System.currentTimeMillis().toString() + ".jpg")
+
+            // 设置源uri及目标uri
+            UCrop.of(uri,Uri.fromFile(avatarFile))
+                // 长宽比
+                .withAspectRatio(1f, 1f)
+                // 图片大小
+                .withMaxResultSize(300, 300)
+                // 配置参数
+                .withOptions(options)
+                .start(this,Constants.REQ_CROP_PHOTO)
+        }
+
+         */
+    }
+
+    private fun obtainSelectPhoto(data: Intent?):Uri?{
+        val result = Matisse.obtainPathResult(data)
+        return Uri.fromFile(File(result[0]))
+    }
+
     inner class ProxyClick{
         fun quitLogin(){
             requestLoginRegisterViewModel.quitLogin(appViewModel.userInfo.value)
@@ -68,8 +130,24 @@ class meUserInfoFragment: BaseFragment<MeUserInfoViewModel, FragmentMeUserInfoBi
                 putInt(Constants.KEY_TYPE, Constants.ChangeMeNickName)
             })
         }
+        @SuppressLint("CheckResult")
         fun toEditIcon(){
-           
+            rxPermission.request(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                .subscribe{
+                    if(it){
+                        Matisse.from(activity)
+                            .choose(MimeType.ofImage())
+                            .maxSelectable(1)
+                            .gridExpectedSize(resources.getDimensionPixelSize(R.dimen.size_120dp))
+                            .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED)
+                            .thumbnailScale(0.85f)
+                            .imageEngine(GlideEngine())
+                            .forResult(Constants.REQ_SELECT_PHOTO)
+                    }
+                }
+            Log.d("select", "选择了一张图片")
         }
         fun toEditSgnature(){
             nav().navigateAction(R.id.action_meUserInfoFragment_to_changeUserInfoFragment, Bundle().apply {
